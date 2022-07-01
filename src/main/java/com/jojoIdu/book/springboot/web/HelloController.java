696f75bf8de39cb1616bcdc4d8d9463dea842dc1
GrowMyself <deae670@daum.net>
2022-07-01 22:05:56 +0900

    test

--- src/main/java/com/jojoIdu/book/springboot/web/HelloController.java
+++ src/main/java/com/jojoIdu/book/springboot/web/HelloController.java
@@ -5,6 +5,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 @RestController // 컨트롤러를 JSON을 반환하는 컨트롤러로 만들어줌
 public class HelloController {
     @GetMapping("/hello")
@@ -13,7 +17,10 @@ public class HelloController {
     }
 
     @GetMapping("/hello/dto")
-    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount,
+                                     HttpServletRequest request) {
+        request.getSession();
+
         return new HelloResponseDto(name, amount);
     }
 }
