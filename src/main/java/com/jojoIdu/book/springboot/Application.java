62bcb682ad9f3ab3a2edd4f638490d154e9951c0
GrowMyself <deae670@daum.net>
2022-07-02 17:16:12 +0900

    Springboob_Book ch8 ing...

--- src/main/java/com/jojoIdu/book/springboot/Application.java
+++ src/main/java/com/jojoIdu/book/springboot/Application.java
@@ -1,5 +1,8 @@
 package com.jojoIdu.book.springboot;
 
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -7,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 //@EnableJpaAuditing // JPA Auditing 활성화
 @SpringBootApplication
+@Slf4j
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
