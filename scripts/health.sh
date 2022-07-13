bb0d7d219490934cbd170dd4abb7399d7f15667b
GrowMyself <deae670@daum.net>
2022-07-14 01:53:13 +0900

    Springboot_Book ch10 395p ing_220713..

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ABSPATH=$(readlink -f $0)
-ABSDIR=$(dirname $ABSPATH)  # 현재 STOP.SH가 속해있는 경로 찾기
-source ${ABSDIR}/profile.sh # 자바의 import 구문과 유사. profile.sh의 function 사용 가능
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
 source ${ABSDIR}/switch.sh
 
 IDLE_PORT=$(find_idle_port)
@@ -19,15 +19,15 @@ do
 
   if [ ${UP_COUNT} -ge 1 ]
   then # $up_count >= 1 ("real" 문자열이 있는지 검증)
-    echo "> Health check 성공"
-    switch_proxy
-    break
+      echo "> Health check 성공"
+      switch_proxy
+      break
   else
-    echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
-    echo "> Health check: ${RESPONSE}"
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check: ${RESPONSE}"
   fi
 
-  if[ ${RETRY_COUNT} -eq 10 ]
+  if [ ${RETRY_COUNT} -eq 10 ]
   then
     echo "> Health check 실패. "
     echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
